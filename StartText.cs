4dcf666a0352720d7d11793baf2831f17d1a30ca
Alan Berberov <92302861+psikh286@users.noreply.github.com>
2025-01-22 20:38:19 +0100

    [REMOVE] outdates scripts;

--- StartText.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-
-[CreateAssetMenu(fileName = "Text", menuName = "Start Text")] 
-public class StartText : ScriptableObject
-{
-    public string[] Texts;
-    public float Delay = 0.4f;
-}
